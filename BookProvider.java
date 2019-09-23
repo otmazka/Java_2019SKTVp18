f5955b66a54c3a344ba4a5185abc9f96a24eba72
otmazka <55397950+otmazka@users.noreply.github.com>
2019-09-23 11:48:07 +0300

    Add files via upload

---
+++ BookProvider.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+import entity.Book;
+import java.util.Scanner;
+
+/**
+ *
+ * @author User
+ */
+public class BookProvider {
+   public Book createBook(){
+       Scanner scanner = new Scanner(System.in);
+       Book book = new Book();
+       System.out.println("Nazvanie knigi:");
+       book.setTitle(scanner.nextLine());
+       System.out.println("Avtor knigi:");
+             book.setAuthor(scanner.nextLine());
+             System.out.println("God izdanija knigi:");
+       
+       book.setYear(new Integer(scanner.nextLine()));
+       return book;
+       
+   }
+}
